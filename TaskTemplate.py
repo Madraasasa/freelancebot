d359c35584a1c65f4d23478802b0d189f30acb18
Maruf <36636180+Madraasasa@users.noreply.github.com>
2020-02-19 06:39:06 +0500

    Add files via upload

--- TaskTemplate.py
+++ TaskTemplate.py
@@ -5,7 +5,7 @@ from buttons import *
 
 def task_name(message,level, bot):
     bot.send_message(message.chat.id, 'Введите описание')
-    # print(message.text)
+    print(message.text)
     bot.register_next_step_handler(message, task_desc,level, message.text, bot)
 
 
@@ -38,7 +38,7 @@ def task_age(message,level, name, desc, sum, sex, bot):
     print(interval)
     if interval[0] and interval[1] and len(interval) == 2:
         bot.send_message(message.chat.id, 'Введите срок выполнения, не менее 5 дней')
-        bot.register_next_step_handler(message, task_date_tasker, level, name, desc, sum, sex, message.text.lower, bot)
+        bot.register_next_step_handler(message, task_date_tasker, level, name, desc, sum, sex, message.text, bot)
     else:
         bot.send_message(message.chat.id, 'Введите возраст, данного формата(возраст-возраст)')
         bot.register_next_step_handler(message, task_age, level, name, desc, sum, message.text, bot)
@@ -56,7 +56,7 @@ def task_date_tasker(message, name,level, desc, sum, sex, age, bot):
 def task_date(message, name,level, desc, sum, sex, age, tasker_dt, bot):
     if message.text.isnumeric() and int(message.text) > 4:
         user_makeup = types.ReplyKeyboardMarkup(True, False)
-        a,b = insert_task(name, desc, sum, sex,age, tasker_dt, message.text, level)
+        a,b = insert_task(name, desc, sum, sex,age, tasker_dt, message.text, level, message.chat.id)
         # print(name, desc, sum, sex,age, tasker_dt, message.text)
         bot.send_message(message.chat.id, f'Задание ID -{a}\n {name}, {desc}, {sum}, {sex},{age}, {tasker_dt}, {message.text}')
         bot.send_message(message.chat.id, b, reply_markup=insert_main(user_makeup))
--- database.py
+++ database.py
@@ -10,17 +10,17 @@ a = []
 #         a.append(row[0])
 
 
-def insert_task(name, desc, sum, sex,age, tasker_dt, t_date, level):
+def insert_task(name, desc, sum, sex,age, tasker_dt, t_date, level, chat_id):
     try:
-        con = p.connect(database='bot', user='postgres', host='localhost', password='VivaProgressio', port=5433)
+        con = p.connect(database='bot', user='postgres', host='localhost', password='VivaProgressio', port=5432)
         cur = con.cursor()
         list_age = age.split('-')
-        cur.execute('select count(*) from public.task')
+        cur.execute('select count(*) from public.botmain_task')
         count = cur.fetchone()
         cur.close()
         cur = con.cursor()
-        cur.execute(f'''INSERT INTO public.task(id, name, "desc", sum, sex, ages, agee, tasker_d, task_date, count_t, level) 
-VALUES ({int(list(count)[0])+1},'{name}', '{desc}', {sum}, '{sex}', {list_age[0]}, {list_age[1]}, {tasker_dt}, {t_date}, 0, '{level}')''')
+        cur.execute(f'''INSERT INTO public.botmain_task(id, name, "desc", sum, sex, ages, agee, tasker_d, t_date, level, creator_id_id) 
+VALUES ({int(list(count)[0])+1},'{name}', '{desc}', {sum}, '{sex}', {list_age[0]}, {list_age[1]}, {tasker_dt}, {t_date}, '{level}', {chat_id})''')
         con.commit()
         con.close()
         return int(list(count)[0])+1, 'Задание успешно добавлено'
@@ -28,11 +28,10 @@ VALUES ({int(list(count)[0])+1},'{name}', '{desc}', {sum}, '{sex}', {list_age[0]
         return 0, 'Произошла ошибка. Повторите попытку'
 
 
-# insert_task('test_name1', 'test_name2', 100, 'Ж', '18-22','5','7', 'medium')
 
 
 def select_task_by_level(level):
-    con = p.connect(database='bot', user='postgres', host='localhost', password='VivaProgressio', port=5433)
+    con = p.connect(database='bot', user='postgres', host='localhost', password='VivaProgressio', port=5432)
     cur = con.cursor()
     cur.execute(f'''select * from public.task where level = '{level}'
     ''')
@@ -49,18 +48,18 @@ def select_task_by_level(level):
 # insert_task('test_name1', 'test_name2', 100, 'Ж', '18-22','5','7', 'lite')
 # select_task_by_level('lite')
 #
-# def insert_task1(name, desc, sum, sex,age, tasker_dt, t_date, level):
-#     con = p.connect(database='bot', user='postgres', host='localhost', password='VivaProgressio', port=5433)
-#     cur = con.cursor()
-#     list_age = age.split('-')
-#     cur.execute('select count(*) from public.task')
-#     count = cur.fetchone()
-#     cur.close()
-#     cur = con.cursor()
-#     cur.execute(f'''INSERT INTO public.task(id, name, "desc", sum, sex, ages, agee, tasker_d, task_date, count_t, level)
-# VALUES ({int(list(count)[0])+1},'{name}', '{desc}', {sum}, '{sex}', {list_age[0]}, {list_age[1]}, {tasker_dt}, {t_date}, 0, '{level}')''')
-#     con.commit()
-#     con.close()
-#
+def insert_task1(name, desc, sum, sex,age, tasker_dt, t_date, level, chat_id):
+    con = p.connect(database='bot', user='postgres', host='localhost', password='VivaProgressio', port=5432)
+    cur = con.cursor()
+    list_age = age.split('-')
+    cur.execute('select count(*) from public.botmain_task')
+    count = cur.fetchone()
+    cur.close()
+    cur = con.cursor()
+    cur.execute(f'''INSERT INTO public.botmain_task(id, name, "desc", sum, sex, ages, agee, tasker_d, t_date, level, creator_id_id) 
+VALUES ({int(list(count)[0])+1},'{name}', '{desc}', {sum}, '{sex}', {list_age[0]}, {list_age[1]}, {tasker_dt}, {t_date}, '{level}', {chat_id})''')
+    con.commit()
+    con.close()
 
-# insert_task('test_name1', 'test_name2', 100, 'Ж', '18-22','5','7', 'lite')
+
+insert_task1('test_name1', 'test_name2', 100, 'мужской', '18-22','5','7', 'medium', 120929625)
